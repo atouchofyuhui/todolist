349aba9c08127842087725395c38bdb33f6466bd
xdou <1161906502@qq.com>
2021-10-18 20:10:49 +0800

    完成简易的tidolist

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,50 @@
 <template>
   <div id="app">
-    <a-input :placeholder="inputValue" class="my_ipt"
-    :value="inputValue"
-    @change="handleInputValue" ></a-input>
+    <a-input
+      :placeholder="inputValue"
+      class="my_ipt"
+      :value="inputValue"
+      @change="handleInputValue"
+      @keyup.enter="addList"
+    ></a-input>
     <a-button type="primary" @click="addList">添加任务</a-button>
 
-    <a-list bordered :dataSource="list" class="dt_list">
+    <a-list bordered :dataSource="infoList" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox @change="checkboxState"> {{item.info}}</a-checkbox>
+        <a-checkbox
+          :checked="item.done"
+          @change="
+            (e) => {
+              checkboxState(e, item.id);
+            }
+          "
+        >
+          {{ item.info }}</a-checkbox
+        >
         <!-- 删除链接 -->
-        <a  @click="removeItem(item.id)">删除</a>
+        <a slot="actions" @click="removeItem(item.id)">删除</a>
       </a-list-item>
 
       <!-- footer -->
-    <div class="footer">
-      <!-- 未完成的任务 -->
-      <span>0条剩余</span>
-      <!-- 操作按钮 -->
-      <a-button-group>
-        <a-button type="primary">全部</a-button>
-        <a-button>未完成</a-button>
-        <a-button>已完成</a-button>
-      </a-button-group>
-      <!--把已完成的任务清空 -->
-      <a >清楚已完成</a>
-    </div>
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务 -->
+        <span>{{ undoneList }}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewKey === 'all' ? 'primary' : 'default'" @click="changeList('all')">全部</a-button>
+          <a-button :type="viewKey === 'undone' ? 'primary' : 'default'" @click="changeList('undone')">未完成</a-button>
+          <a-button :type="viewKey === 'done' ? 'primary' : 'default'" @click="changeList('done')">已完成</a-button>
+        </a-button-group>
+        <!--把已完成的任务清空 -->
+        <a @click="clearDoneList">清除已完成</a>
+      </div>
     </a-list>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'App',
   data () {
@@ -39,13 +52,13 @@ export default {
       // list: []
     }
   },
-  components: {
-  },
+  components: {},
   created () {
     this.$store.dispatch('getList')
   },
   computed: {
-    ...mapState(['list', 'inputValue'])
+    ...mapState(['list', 'inputValue', 'viewKey']),
+    ...mapGetters(['undoneList', 'infoList'])
   },
   methods: {
     // 监听输入框中文本的变化
@@ -64,24 +77,37 @@ export default {
     removeItem (id) {
       // console.log(id)
       this.$store.commit('removeItem', id)
+      this.$message.success('删除成功')
     },
     // 修改任务状态
-    checkboxState (e) {
-      console.log(e.target.checked)
-      // const param = {
-      // }
+    checkboxState (e, id) {
+      console.log(e.target.checked, id)
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      this.$store.commit('changeStatus', param)
+    },
+    // 清除已完成的任务
+    clearDoneList () {
+      this.$store.commit('clearDone')
+    },
+    // 切换任务列表
+    changeList (key) {
+      // console.log(key)
+      this.$store.commit('changeList', key)
     }
   }
 }
 </script>
 
-<style scope>
+<style scoped>
 #app {
- padding: 10px;
+  padding: 10px;
 }
 
 .my_ipt {
-  width: 500px;
+  width: 400px;
   margin-right: 10px;
 }
 .dt_list {
--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,8 @@ export default new Vuex.Store({
     list: [],
     // 输入框中的内容
     inputValue: '',
-    nextId: 5
+    nextId: 5,
+    viewKey: 'all'
   },
   mutations: {
     initList (state, list) {
@@ -28,19 +29,34 @@ export default new Vuex.Store({
         done: false
       }
       // 加入数组
-      this.state.list.push(obj)
+      state.list.push(obj)
       state.nextId++
       // 清空输入框
-      this.state.inputValue = ''
+      state.inputValue = ''
     },
     // 删除任务
     removeItem (state, id) {
       // 找到要删除的项
-      const r = this.state.list.findIndex(i => i.id === id)
+      const r = state.list.findIndex(i => i.id === id)
       if (r !== -1) {
         // 删除找到的那个项
-        this.state.list.splice(r, 1)
+        state.list.splice(r, 1)
       }
+    },
+    // 变更状态
+    changeStatus (state, param) {
+      const r = state.list.findIndex(i => i.id === param.id)
+      if (r !== -1) {
+        state.list[r].done = param.status
+      }
+    },
+    // 清除已完成的任务
+    clearDone (state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    // 切换任务列表
+    changeList (state, key) {
+      state.viewKey = key
     }
   },
   actions: {
@@ -52,5 +68,23 @@ export default new Vuex.Store({
     }
   },
   modules: {
+  },
+  getters: {
+    // 统计剩余未完成的任务
+    undoneList (state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    // 切换任务列表
+    infoList (state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'undone') {
+        return state.list.filter(x => !x.done)
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter(x => x.done)
+      }
+    }
   }
 })
